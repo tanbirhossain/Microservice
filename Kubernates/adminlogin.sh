3fd2b577a730067ba4f86e540b2ae705da24bf5e
Mohammed Tanbir Hossain <ovibhuiyan43@gmail.com>
2021-03-29 17:02:02 +0900

    kubernate

---
+++ Kubernates/adminlogin.sh
@@ -0,0 +1,4 @@
+# http://localhost:8001/api/v1/namespaces/kubernetes-dashboard/services/https:kubernetes-dashboard:/proxy/#/storageclass?namespace=default
+kubectl -n kubernetes-dashboard get secret $(kubectl -n kubernetes-dashboard get sa/admin-user -o jsonpath="{.secrets[0].name}") -o go-template="{{.data.token | base64decode}}"
+
+
